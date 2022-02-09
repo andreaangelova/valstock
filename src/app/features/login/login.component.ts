5347d98b723cf218111ca5f3aa798fc3810b8537
Andrea Angelova <Andrea.Angelova@opengi.mk>
2022-02-09 13:34:51 +0100

    add log in form

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.less']
+  styleUrls: ['./login.component.less'],
 })
 export class LoginComponent implements OnInit {
+  loginForm = this.formBuilder.group({
+    name: '',
+    password: '',
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login(): void {
+    console.log(this.loginForm.value);
+    this.loginForm.reset();
   }
-
 }
--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // extend this module with the additional log in features: eg. register, reset password...
 const components = [LoginComponent];
 @NgModule({
   declarations: components,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: components,
 })
 export class LoginModule {}
