f46fd5402a2eafd5fff0b93bc796e0d736e74345
Andrea Angelova <Andrea.Angelova@opengi.mk>
2022-02-11 19:40:20 +0100

    add my albums button

--- src/app/core/nav/nav.component.ts
+++ src/app/core/nav/nav.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.less']
+  styleUrls: ['./nav.component.less'],
 })
 export class NavComponent implements OnInit {
+  loggedIn = false;
+  subscription: Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private loginService: LoginService) {
+    this.subscription = this.loginService.loggedIn.subscribe(
+      (data) => (this.loggedIn = data)
+    );
   }
 
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
