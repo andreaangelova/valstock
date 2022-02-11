80c8d00eddd93c268da359de85a71936bbc2f51f
Andrea Angelova <Andrea.Angelova@opengi.mk>
2022-02-11 17:58:24 +0100

    set up item page

--- src/app/features/dashboard/dashboard-routing.module.ts
+++ src/app/features/dashboard/dashboard-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { ItemComponent } from './item/item.component';
 
-const routes: Routes = [{ path: '', component: DashboardComponent }];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'item/:id', component: ItemComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Item } from 'src/app/core/models/item';
 import { ItemsService } from 'src/app/core/services/items.service';
@@ -12,7 +13,7 @@ export class DashboardComponent implements OnInit {
   items: Item[] = [];
   subscription: Subscription;
 
-  constructor(private itemsService: ItemsService) {
+  constructor(private itemsService: ItemsService, private router: Router) {
     this.subscription = this.itemsService.items.subscribe((data) => {
       this.items = data;
       if (data.length === 0) this.itemsService.getItems();
@@ -20,6 +21,10 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  goToItem(item: Item): void {
+    this.router.navigate(['dashboard/item', item.id]);
+  }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
