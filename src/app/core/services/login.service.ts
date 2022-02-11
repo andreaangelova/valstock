4acc06f09cca4528fcbd822407037c85ae5a9472
Andrea Angelova <Andrea.Angelova@opengi.mk>
2022-02-11 19:39:14 +0100

    add login state

--- src/app/core/services/login.service.ts
+++ src/app/core/services/login.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
+  public loggedIn = new BehaviorSubject<boolean>(false);
   constructor() {}
 
   logIn(user: Object) {
+    this.loggedIn.next(true);
     localStorage.setItem('user', JSON.stringify(user));
   }
 
   logOut() {
+    this.loggedIn.next(false);
     localStorage.removeItem('user');
   }
 
   isLoggedIn() {
-    return localStorage.getItem('user') ? true : false;
+    let loggedIn = localStorage.getItem('user') ? true : false;
+    this.loggedIn.next(loggedIn);
+    return loggedIn;
   }
 }
